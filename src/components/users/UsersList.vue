e6926f652b17d1010c571485b0b379e3c2979469
Galen Lockwood <galenlockwood@gmail.com>
2021-08-09 13:35:00 -0400

    Update UsersList.vue
    added programmatic routing example

--- src/components/users/UsersList.vue
+++ src/components/users/UsersList.vue
@@ -1,25 +1,34 @@
 <template>
+  <button @click="confirmInput">Confirm</button>
   <ul>
     <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
   </ul>
 </template>
 
 <script>
-import UserItem from './UserItem.vue';
+    import UserItem from './UserItem.vue';
 
-export default {
-  components: {
-    UserItem,
-  },
-  inject: ['users'],
-};
+    export default {
+        components: {
+            UserItem,
+        },
+        inject: ['users'],
+        methods: {
+            confirmInput() {
+                // via a function on a button; go to new page
+                this.$router.push('/teams')
+                    // this.$router.back();
+                    // this.$router.forward();
+            }
+        }
+    };
 </script>
 
 <style scoped>
-ul {
-  list-style: none;
-  margin: 2rem auto;
-  max-width: 20rem;
-  padding: 0;
-}
+    ul {
+        list-style: none;
+        margin: 2rem auto;
+        max-width: 20rem;
+        padding: 0;
+    }
 </style>
